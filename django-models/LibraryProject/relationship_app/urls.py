d8363232a77b58537722b26bc93325f032c39dd3
Emane-11 <alhachimi2000@gmail.com>
2025-07-23 22:10:22 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,14 @@
 from django.urls import path
-from .views import list_books
-from .views import RegisterView
-from .views import CustomLogoutView
+from .views import list_books, RegisterView, CustomLogoutView, LibraryDetailView
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),  # FBV
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV
-     # Auth URLs
-    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-]
+    path('books/', list_books, name='list_books'),  # FBV
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # CBV
 
+    # Auth URLs
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+]
 
