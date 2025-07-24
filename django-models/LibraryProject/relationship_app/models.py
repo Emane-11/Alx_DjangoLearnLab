3d772e6d7d9d74dc1af226c3aba202e4933511b3
Emane-11 <alhachimi2000@gmail.com>
2025-07-24 16:42:52 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -7,12 +7,25 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-class Book(models.Model):
+'''class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
-        return self.title
+        return self.title'''
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    # other fields...
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
+
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
