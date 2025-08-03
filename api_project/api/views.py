25cfa28b5e1094c96efc687453d72c0c4e2954be
Imane Elhachimi <alhachimi2000@gmail.com>
2025-08-03 10:02:56 +0100

    Implement CRUD operations for Book model using DRF ViewSet and Router

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+''' ⬆️ This single class handles:
+
+GET /books_all/ → list
+
+GET /books_all/<id>/ → retrieve
+
+POST /books_all/ → create
+
+PUT /books_all/<id>/ → update
+
+DELETE /books_all/<id>/ → destroy'''
+
