aa9181117fa509180a79fa222efc926cb89dec42
Imane Elhachimi <alhachimi2000@gmail.com>
2025-08-10 18:53:22 +0100

    updated api/views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -13,6 +13,7 @@ def author_list(request):
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 """
 View Classes:
