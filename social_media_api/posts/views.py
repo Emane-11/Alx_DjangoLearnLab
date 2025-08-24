99ee39243951d9081dc1d299223fa88737ac4498
Imane Elhachimi <alhachimi2000@gmail.com>
2025-08-24 18:26:51 +0100

    Updated posts/views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,44 +1,30 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework import generics, viewsets
+from rest_framework import permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsAuthorOrReadOnly
-from rest_framework import generics
-
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter]
-    filterset_fields = ['author']
-    search_fields = ['title', 'content']
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
+    queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
-
-    def get_queryset(self):
-        # This ensures comments are specific to a post
-        return self.queryset.filter(post_id=self.kwargs['post_pk'])
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
-        # Pass the post to the serializer context
-        post = Post.objects.get(pk=self.kwargs['post_pk'])
-        serializer.save(author=self.request.user, post=post)
+        serializer.save(author=self.request.user)
 
 class UserFeedView(generics.ListAPIView):
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        # Get the list of users the current user is following
-        followed_users = self.request.user.following.all()
-        # Return posts from those users, ordered by creation date
-        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        # Retrieve the user's following list.
+        following_users = self.request.user.following.all()
+        # Filter posts from those users and order them.
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
