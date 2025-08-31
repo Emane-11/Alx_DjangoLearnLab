f1f9c3eec2cb255c2a4ce9bf4163e37a15fa1faa
Emane-11 <alhachimi2000@gmail.com>
2025-08-31 20:34:56 +0100

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,10 +5,11 @@ from .views import PostViewSet, CommentViewSet, UserFeedView, like_post, unlike_
 router = SimpleRouter()
 router.register('posts', PostViewSet)
 router.register('comments', CommentViewSet)
+# Register the UserFeedView with the router to automatically generate its URL patterns.
+router.register('feed', UserFeedView)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('feed/', UserFeedView.as_view(), name='user_feed'),
     path('posts/<int:pk>/like/', like_post, name='like_post'),
     path('posts/<int:pk>/unlike/', unlike_post, name='unlike_post'),
 ]
